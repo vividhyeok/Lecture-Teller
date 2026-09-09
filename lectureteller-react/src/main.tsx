003a8dd24d54168ddde84c736182c8d6b7cdc15d
Minhyeok Kim <basakgrape@gmail.com>
2026-09-09 17:02:09 +0900

    fix: use compact stable UI entrypoint

--- lectureteller-react/src/main.tsx
+++ lectureteller-react/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppStable from './AppStable.tsx'
+import AppStable from './AppStable2.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
